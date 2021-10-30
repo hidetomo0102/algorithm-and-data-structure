cb384a028cac20797087a4e53385042dd80e6a94
hidetomo0102 <hidenorishio@icloud.com>
2021-10-30 10:40:19 +0900

    キャッシュデコレーター問題

--- lesson.py
+++ lesson.py
@@ -1,27 +1,25 @@
 """
-ある工場では、 数直線上に N 個のロボットが設置されています
-ロボット i は座標 Xi に設置されており、数直線の正負の方向にそれぞれ長さ Liの腕を伸ばすことができます
-これらのロボットのうちいくつか (0個以上) を取り除き、 残ったどの 2 つのロボットについても、腕が動く範囲が共通部分を持たないようにしたいと思います
-（ただし、各 i (1≤i≤N) に対して、 ロボット i の腕が動く範囲とは 数直線上の座標が Xi−Li より大きく Xi+Li未満の部分を指します）
-取り除かずに残せるロボットの個数の最大値を求めてください
-
-INPUT:  N
-        X(1), L(1)
-        ...
-        X(n), L(n)
+キャッシュデコレーターを作成
 """
-n = int(input())
-ls = []
+from time import time
 
-for i in range(n):
-    x, l = map(int, input().split())
-    # [始点, 終点]
-    ls.append([x - l, x + l])
 
-ls = sorted(ls, key=lambda x: x[1])
+def cache_dec(f):
+    cache = {}
 
-ans = n
-for i in range(1, n):
-    if ls[i][0] < ls[i - 1][1]:
-        ls[i][1] = ls[i - 1][1]
-        ans -= 1
+    def _wrapper(n):
+        if n not in cache:
+            cache[n] = f(n)
+            return f(n)
+        else:
+            return cache[n]
+
+    return _wrapper
+
+
+@cache_dec
+def long_func(num: int) -> int:
+    r = 0
+    for i in range(100):
+        r += num * i
+    return r
