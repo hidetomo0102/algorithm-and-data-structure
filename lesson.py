f5a42ee745a301f0cdf129fdb0f2d2b16dd7d41a
hidetomo0102 <hidenorishio@icloud.com>
2021-10-30 11:16:55 +0900

    hash table quiz2

--- lesson.py
+++ lesson.py
@@ -1,25 +1,31 @@
 """
-キャッシュデコレーターを作成
+ある値が2つのリストに出現したとき、出現回数が少ない方のリストから削除する
+（もし両方のリストに同数ある場合はスキップ）
+
+INPUT X: [1, 2, 3, 4, 4, 5, 5, 8, 10] Y: [4, 5, 5, 5, 6, 7, 8, 8, 10]
+  =>  X: [1, 2, 3, 4, 4, 10] Y: [5, 5, 5, 6, 7, 8, 8, 10]
 """
-from time import time
+from typing import List
+from collections import Counter
+
+ans_x = []
+ans_y = []
 
 
-def cache_dec(f):
-    cache = {}
+def min_count_remove(x: List[int], y: List[int]):
+    x_counter = Counter(x)
+    y_counter = Counter(y)
 
-    def _wrapper(n):
-        if n not in cache:
-            cache[n] = f(n)
-            return f(n)
-        else:
-            return cache[n]
+    for x_key, x_value in x_counter.items():
+        y_value = y_counter.get(x_key)
+        if y_value:
+            if x_value < y_value:
+                # そのkey以外の要素でリストを置き換える
+                x[:] = [i for i in x if i != x_key]
+            elif x_value > y_value:
+                y[:] = [i for i in y if i != x_key]
+    return x, y
 
-    return _wrapper
 
-
-@cache_dec
-def long_func(num: int) -> int:
-    r = 0
-    for i in range(100):
-        r += num * i
-    return r
+x, y = min_count_remove([1, 2, 3, 4, 4, 5, 5, 8, 10], [4, 5, 5, 5, 6, 7, 8, 8, 10])
+print(x, y)
