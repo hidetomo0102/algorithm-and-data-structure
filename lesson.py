07c2dd303ef0b5e63751d663c481f0b54bdb2721
hidetomo0102 <hidenorishio@icloud.com>
2021-11-02 11:10:39 +0900

    リスト要素の重複を削除する

--- lesson.py
+++ lesson.py
@@ -1,28 +1,36 @@
 """
-1. Maximum subarray sum
-Input: [1, -2, 3, 6, -1, 2, 4, -5, 2]
-Output: 14, (3, 6, -1, 2, 4)
-
-2. Maximum circular subarray sum
-Input: [1, -2, 3, 6, -1, 2, 4, -5, 2]
-Output: 15, (2, 1, -2, 3, 6, -1, 2, 4)
+リスト内の要素を重複させない
+[1, 3, 3, 5, 7, 7, 7, 10, 12, 12 ,15] => [1, 3, 5, 7, 10, 12, 15]
 """
 from typing import List
 
 
-def get_sequence_sum(numbers: List[int], operator=max) -> int:
-    result_sequence, sum_sequence = 0, 0
+def delete_duplicate_v1(numbers: List[int]):
+    tmp = []
     for num in numbers:
-        sum_sequence = operator(num, sum_sequence + num)
-        result_sequence = operator(result_sequence, sum_sequence)
-    return result_sequence
+        if num not in tmp:
+            tmp.append(num)
+    numbers[:] = tmp
 
 
-def get_max_circular_sequence_sum(numbers: List[int]) -> int:
-    max_sequence_sum = get_sequence_sum(numbers)
-    max_wrap_sequence = sum(numbers) - get_sequence_sum(numbers, operator=min)
-    return max(max_sequence_sum, max_wrap_sequence)
+def delete_duplicate_v2(numbers: List[int]):
+    tmp = [numbers[0]]
+    i, len_num = 0, len(numbers) - 1
+    while i < len_num:
+        if numbers[i] != numbers[i + 1]:
+            tmp.append(numbers[i + 1])
+        i += 1
+    numbers[:] = tmp
 
 
-a = get_max_circular_sequence_sum([1, -2, 3, 6, -1, 2, 4, -5, 2])
-print(a)  # 15
+def delete_duplicate_v3(numbers: List[int]):
+    i = len(numbers) - 1
+    while i > 0:
+        if numbers[i] == numbers[i - 1]:
+            numbers.pop(i)
+        i -= 1
+
+
+numbers = [1, 3, 3, 5, 7, 7, 7, 10, 12, 12, 15]
+delete_duplicate_v3(numbers)
+print(numbers)  # [1, 3, 5, 7, 10, 12, 15]
