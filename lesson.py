9b786977f3b1f5b57de4d4250a416ca32b41bfe4
hidetomo0102 <hidenorishio@icloud.com>
2021-11-05 10:46:26 +0900

    palindrome

--- lesson.py
+++ lesson.py
@@ -1,40 +1,60 @@
 """
-二部グラフの判定
+1. Check palindrome
+aba => True
+abc => False
+racecar => True
 
-N := 頂点の数
-M := 辺の数
+2. Find palindrome
+abcracecarda => cec, aceca, rcecar
 """
-from collections import defaultdict
-
-N, M = map(int, input().split())
-d = defaultdict(set)
-
-for i in range(M):
-    a, b = list(map(int, input().split()))
-
-    d[a].add(b)
-    d[b].add(a)
-
-color = [0 for i in range(N + 1)]
-res = "Yes"
+from typing import Generator
 
 
-def dfs(v, c):
-    queue = []
-
-    def _dfs(v, c, queue: list):
-        queue.append(v)
-
-        for i in list(d[v]):
-            if color[i] == c:
-                return "No"
-            elif i not in queue:
-                color[i] = c * -1
-                _dfs(i, -c, queue)
-        return "Yes"
-
-    return _dfs(v, c, queue)
+def check_palindrome_v1(chars: str) -> bool:
+    if chars == chars[::-1]:
+        return True
+    return False
 
 
-color[1] = 1
-print(dfs(1, 1))
+def check_palindrome_v2(chars: str) -> bool:
+    len_char = len(chars)
+    if not len_char:
+        return False
+    if len_char == 1:
+        return True
+
+    start, end = 0, len_char - 1
+    while start < end:
+        if chars[start] != chars[end]:
+            return False
+        start += 1
+        end -= 1
+
+    return True
+
+
+def find_palindrome(chars: str) -> Generator:
+    len_chars = len(chars)
+
+    if not len_chars:
+        yield
+
+    if len_chars == 1:
+        yield chars
+
+    def _find_palindrome(chars: str, left: int, right: int) -> Generator:
+        while 0 <= left and right <= len(chars) - 1:
+            if chars[left] != chars[right]:
+                break
+
+            yield chars[left:right + 1]
+            left -= 1
+            right += 1
+
+    for i in range(1, len_chars - 1):
+        yield from _find_palindrome(chars, i - 1, i + 1)  # 奇数の場合
+        yield from _find_palindrome(chars, i - 1, i)  # 偶数の場合
+
+
+for s in find_palindrome("cabac"):
+    print(s)
