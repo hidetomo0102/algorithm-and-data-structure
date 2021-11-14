24838b888327161e2b785def9175936ac9ebc25f
hidetomo0102 <hidenorishio@icloud.com>
2021-11-14 12:37:29 +0900

    ハノイの塔

---
+++ quiz/puzzle.py
@@ -0,0 +1,15 @@
+"""
+ハノイの塔
+"""
+
+
+def hanoi(disk: int, src: str, dest: str, support: str):
+    if disk < 1:
+        return
+
+    hanoi(disk - 1, src, support, dest)
+    print(f'move {disk} from {src} to {dest}')
+    hanoi(disk - 1, support, dest, src)
+
+
+hanoi(3, 'A', 'C', 'B')
