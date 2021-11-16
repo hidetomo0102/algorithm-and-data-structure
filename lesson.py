cb99f126142dd6b425064988fec3a88157a42d16
hidetomo0102 <hidenorishio@icloud.com>
2021-11-16 11:26:47 +0900

    27.Remove Element

--- lesson.py
+++ lesson.py
@@ -1,16 +1,10 @@
 from typing import List
 
 
-def remove_duplicates(nums: List[int]) -> int:
-    length = len(nums)
-    duplicate_count = 0
-    for i in reversed(range(1, length)):
-        if nums[i] == nums[i - 1]:
-            nums.pop(i)
-            duplicate_count += 1
-
-    return length - duplicate_count
+def removeElement(nums: List[int], val: int) -> int:
+    nums[:] = [i for i in nums if i != val]
+    return len(nums)
 
 
-a = remove_duplicates([1, 1])
-b = remove_duplicates([0, 0, 1, 1, 1, 2, 2, 3, 3, 4])
+a = removeElement([3, 2, 2, 3], 3)
+b = removeElement([0, 1, 2, 2, 3, 0, 4, 2], 2)
