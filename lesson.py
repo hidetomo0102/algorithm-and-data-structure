d13a9e6aead98c17e7b122e6d26f217eebe04e61
hidetomo0102 <hidenorishio@icloud.com>
2021-11-18 12:56:41 +0900

    35.Search Insert Position

--- lesson.py
+++ lesson.py
@@ -1,11 +1,30 @@
-def strStr(haystack: str, needle: str) -> int:
-    if needle not in haystack:
-        return -1
-
-    return haystack.find(needle)
+from typing import List
 
 
-a = strStr("hello", "ll")
-b = strStr("", "")
-print(a)
-print(b)
+def searchInsert(nums: List[int], target: int) -> int:
+    if target <= nums[0]:
+        return 0
+
+    if target > nums[len(nums) - 1]:
+        return len(nums)
+
+    def _search(left: int, right: int, target: int):
+        if left >= right:
+            if nums[left] < target:
+                return left + 1
+            else:
+                return left
+
+        mid = (left + right) // 2
+        if nums[mid] == target:
+            return mid
+        elif nums[mid] < target:
+            return _search(mid + 1, right, target)
+        else:
+            return _search(left, mid - 1, target)
+
+    return _search(1, len(nums) - 1, target)
+
+
+c = searchInsert([1, 2, 4, 6, 7], 3)
+print(c)
