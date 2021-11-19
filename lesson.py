bf157c02980e006850f43b2abab3f446850f9a91
hidetomo0102 <hidenorishio@icloud.com>
2021-11-19 18:52:06 +0900

    58.Length of Last Word

--- lesson.py
+++ lesson.py
@@ -1,18 +1,7 @@
-from typing import List
+def lengthOfLastWord(s: str) -> int:
+    strs = s.split()
+    return len(strs[-1])
 
 
-def maxSubArray(nums: List[int]) -> int:
-    result_sequence, sum_sequence = nums[0], 0
-    for num in nums:
-        sum_sequence = max(num, sum_sequence + num)
-        result_sequence = max(result_sequence, sum_sequence)
-
-    return result_sequence
-
-
-a = maxSubArray([-2, 1, -3, 4, -1, 2, 1, -5, 4])
-b = maxSubArray([5, 4, -1, 7, 8])
-c = maxSubArray([-1])
+a = lengthOfLastWord("   fly me   to   the moon  ")
 print(a)
-print(b)
-print(c)
