15bc00c17531bb139189f9380b73e2b831caf2a6
hidetomo0102 <hidenorishio@icloud.com>
2021-11-21 11:52:02 +0900

    70.Climbing Stairs

--- lesson.py
+++ lesson.py
@@ -1,17 +1,5 @@
-def mySqrt(x: int) -> int:
-    root = -1
-    start = 0
-    end = x
-
-    while start <= end:
-        mid = start + (end - start) // 2
-        if mid * mid == x:
-            root = mid
-            break
-        if mid * mid > x:
-            end = mid - 1
-        else:
-            start = mid + 1
-            root = mid
-
-    return root
+def climbStairs(n: int) -> int:
+    s = [1, 1]
+    for i in range(2, n + 1):
+        s.append(s[-1] + s[-2])
+    return s[-1]
