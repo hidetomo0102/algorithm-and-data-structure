946669c2dea6f187fa865dc98afd8e6d90c27555
hidetomo0102 <hidenorishio@icloud.com>
2021-11-28 13:14:51 +0900

    111.Minimum Depth of Binary Tree

--- lesson.py
+++ lesson.py
@@ -1,6 +1,6 @@
-# 110.Balanced Binary Tree
+# 111.Minimum Depth of Binary Tree
+
 from typing import Optional
-from collections import deque, defaultdict
 
 
 class TreeNode:
@@ -11,30 +11,16 @@ class TreeNode:
 
 
 class Solution:
-    def isBalanced(self, root: Optional[TreeNode]) -> bool:
+    def minDepth(self, root: Optional[TreeNode]) -> int:
         if not root:
-            return True
+            return 0
 
-        s = deque()
-        s.appendleft((root, -1, 0))
+        left = self.minDepth(root.left)
+        right = self.minDepth(root.right)
 
-        r = deque()
-
-        while s:
-            node, parent, LR = s.popleft()
-            r.appendleft((node, parent, LR))
-
-            if node.right:
-                s.appendleft((node.right, node, 1))
-            if node.left:
-                s.appendleft((node.left, node, 0))
-
-        heights = defaultdict(lambda: [0, 0])
-        while r:
-            node, parent, LR = r.popleft()
-            node_heights = heights.get(node, [0, 0])
-            if abs(node_heights[0] - node_heights[1]) > 1:
-                return False
-            heights[parent][LR] = max(node_heights) + 1
-
-        return True
+        if left == 0:
+            return right + 1
+        elif right == 0:
+            return left + 1
+        else:
+            return min(left, right) + 1
