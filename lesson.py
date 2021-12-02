c767d66da237e4fe2a35cb0e5f454bf1e728b575
hidetomo0102 <hidenorishio@icloud.com>
2021-12-02 11:46:52 +0900

    121.Best Time to Buy and Sell Stock

--- lesson.py
+++ lesson.py
@@ -1,16 +1,12 @@
-# 119.Pascal's Triangle II
+# 121.Best Time to Buy and Sell Stock
 from typing import List
 
 
-def getRow(rowIndex: int) -> List[int]:
-    data = [[1] * (i + 1) for i in range(rowIndex + 1)]
+def maxProfit(prices: List[int]) -> int:
+    profit = 0
 
-    for line in range(2, rowIndex + 1):
-        for i in range(1, line):
-            data[line][i] = data[line - 1][i - 1] + data[line - 1][i]
+    for i in range(1, len(prices)):
+        profit = max(profit, prices[i] - prices[i - 1])
+        prices[i] = min(prices[i], prices[i - 1])
 
-    return data[rowIndex]
-
-
-a = getRow(1)
-print(a)
+    return profit
