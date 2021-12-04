c79d42877e1834fdcff7afbe680991537f348813
hidetomo0102 <hidenorishio@icloud.com>
2021-12-04 23:24:50 +0900

    141.Linked List Cycle

--- lesson.py
+++ lesson.py
@@ -1,9 +1,22 @@
-# 136.Single Number
-from typing import List
-from collections import Counter
+# 141.Linked List Cycle
+from typing import Optional
 
 
-def singleNumber(nums: List[int]) -> int:
-    counter = Counter(nums)
-    c = counter.most_common()
-    return c[-1][0]
+class ListNode:
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+
+
+def hasCycle(head: Optional[ListNode]) -> bool:
+    if not head:
+        return False
+
+    p = head
+    while p:
+        if p.val == "v":
+            return True
+        p.val = "v"
+        p = p.next
+
+    return False
