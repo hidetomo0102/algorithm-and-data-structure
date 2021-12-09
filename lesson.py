b7c483a4dec51cfb89c3f3c91dbd625b7f615355
hidetomo0102 <hidenorishio@icloud.com>
2021-12-09 13:59:49 +0900

    160.Intersection of Two Linked Lists

--- lesson.py
+++ lesson.py
@@ -1,25 +1,27 @@
-# 155.Min Stack
+# 160.Intersection of Two Linked Lists
+from typing import Optional
 
 
-class MinStack:
+class ListNode:
+    def __init__(self, x):
+        self.val = x
+        self.next = None
 
-    def __init__(self):
-        self.stack = []
-        self.min_stack = []
 
-    def push(self, val: int) -> None:
-        self.stack.append(val)
-        if not self.min_stack:
-            self.min_stack.append(val)
-        else:
-            self.min_stack.append(min(val, self.min_stack[-1]))
+class Solution:
+    def getIntersectionNode(self, headA: ListNode, headB: ListNode) -> Optional[ListNode]:
+        if not headA or not headB:
+            return None
 
-    def pop(self) -> None:
-        self.stack.pop()
-        self.min_stack.pop()
+        nodes = set()
+        p1 = headA
+        while p1:
+            nodes.add(p1)
+            p1 = p1.next
+        p1 = headB
+        while p1:
+            if p1 in nodes:
+                return p1
+            p1 = p1.next
 
-    def top(self) -> int:
-        return self.stack[-1]
-
-    def getMin(self) -> int:
-        return self.min_stack[-1]
+        return None
