2049ea95568c6c49f0e5476c63f31cb2a71a007f
hidetomo0102 <hidenorishio@icloud.com>
2021-12-14 13:57:42 +0900

    5.Longest Palindromic Substring

--- lesson.py
+++ lesson.py
@@ -1,19 +1,30 @@
-# 3.Longest Substring Without Repeating Characters
+# 5.Longest Palindromic Substring
 
 
-def lengthOfLongestSubstring(s: str) -> int:
-    temp = ""
-    lengths = []
+def longestPalindrome(s: str) -> str:
+    def expand(l, r):
+        while l >= 0 and r < len(s):
+            if s[l] != s[r]:
+                break
+            l -= 1
+            r += 1
 
-    if len(s) < 2:
-        return len(s)
+        return min(l + 1, len(s) - 1), max(r - 1, 0)
 
-    for l in s:
-        if l in temp:
-            lengths.append(len(temp))
-            temp = temp[temp.index(l) + 1:] + l
-        else:
-            temp += l
+    max_len = 0
+    maxl = maxr = 0
 
-    lengths.append(len(temp))
-    return max(lengths)
+    for i in range(len(s)):
+        l1, r1 = expand(i, i + 1)
+        l2, r2 = expand(i, i)
+
+        if r1 - l1 > max_len:
+            max_len = r1 - l1
+            maxl = l1
+            maxr = r1
+        elif r2 - l2 > max_len:
+            max_len = r2 - l2
+            maxl = l2
+            maxr = r2
+
+    return s[maxl:maxr + 1]
