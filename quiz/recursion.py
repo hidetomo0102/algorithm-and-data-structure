e004ede0506d739260499ce1ee61e3de5dd919df
hidetomo0102 <hidenorishio@icloud.com>
2021-12-16 12:38:01 +0900

    find palindromes

--- quiz/recursion.py
+++ quiz/recursion.py
@@ -13,3 +13,55 @@ def all_perms(elements: List[int]) -> List[List[int]]:
         for perm in all_perms(elements[1:]):
             for i in range(len(elements)):
                 yield perm[:i] + elements[0:1] + perm[i:]
+
+
+"""
+1. Check palindrome
+aba => True
+abc => False
+racecar => True
+
+2. Find palindrome
+abcracecarbda => cec, aceca, racecar
+"""
+
+
+def is_palindrome(char: str) -> bool:
+    if not char:
+        return False
+
+    if len(char) == 1:
+        return True
+
+    start, end = 0, len(char) - 1
+    while start < end:
+        if char[start] != char[end]:
+            return False
+        start += 1
+        end -= 1
+
+    return True
+
+
+def find_palindrome(char: str, left: int, right: int):
+    while 0 <= left and right <= len(char) - 1:
+        if char[left] != char[right]:
+            break
+
+        yield char[left: right + 1]
+        left -= 1
+        right += 1
+
+
+def find_all_palindrome(strings: str):
+    length = len(strings)
+
+    if not length:
+        yield
+
+    if length == 1:
+        yield strings
+
+    for i in range(1, length - 1):
+        yield from find_palindrome(strings, i - 1, i + 1)
+        yield from find_palindrome(strings, i - 1, i)
