20df98db8d34102c4e79e402c1f818acee3da4e9
hidetomo0102 <hidenorishio@icloud.com>
2021-12-17 12:05:02 +0900

    7.Reverse Integer

--- lesson.py
+++ lesson.py
@@ -1,28 +1,16 @@
-# 6.Zigzag Conversion
-def convert(s: str, numRows: int) -> str:
-    if numRows == 1:
-        return s
-
-    res = [""] * numRows
-
-    j = 0
-    k = numRows - 1
-    for i in range(len(s)):
-        if j < numRows:
-            res[j] += s[i]
-            j += 1
-
-        if j >= numRows:
-            if 1 <= k <= numRows - 2:
-                res[k] += s[i]
-            k -= 1
-
-        if k == 0 and j >= numRows:
-            j = 0
-            k = numRows - 1
-
-    return "".join(res)
+# 7.Reverse Integer
 
 
-a = convert("PAYPALISHIRING", 3)
-print(a)
+def reverse(x: int) -> int:
+    if x == 0:
+        return x
+
+    if x > 0:
+        nums = int(str(x)[::-1])
+    else:
+        nums = -int(str(x)[:-2])
+
+    if abs(nums) > pow(2, 31):
+        return 0
+
+    return nums
