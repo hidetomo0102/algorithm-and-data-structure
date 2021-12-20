a3f40599c40f65c9b8a49ffc4ebd795f6a2e0229
hidetomo0102 <hidenorishio@icloud.com>
2021-12-20 11:52:32 +0900

    ABC128 C - Switches

--- lesson.py
+++ lesson.py
@@ -1,10 +1,31 @@
-# ABC127 C - Prison
+# ABC128 C - Switches
+from itertools import product
+
+
+def judge(pro):
+    # 電球iに繋がったスイッチが押された回数
+    count = [0] * M
+    for i in range(N):
+        if not pro[i]:
+            continue
+        for x in S[i]:
+            count[x] += 1
+
+    for p, c in zip(P, count):
+        if c % 2 != p:
+            return 0
+    return 1
+
+
 N, M = map(int, input().split())
+S = [[] for _ in range(M)]
+for i in range(M):
+    k, *T = map(int, input().split())
+    for x in T:
+        S[x - 1].append(i)
 
-l, r = map(int, input().split())
-for _ in range(M - 1):
-    L, R = map(int, input().split())
-    l = max(l, L)
-    r = min(r, R)
+P = list(map(int, input().split()))
 
-print(max(0, r - l + 1))
+ans = 0
+for pro in product((0, 1), repeat=N):
+    ans += judge(pro)
